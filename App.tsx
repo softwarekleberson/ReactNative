90b229e711d366497b79b9a5d99eb43d68934103
softwarekleberson <santossilvakleberson@gmail.com>
2023-05-08 17:02:01 -0300

    Commit até a aula 4 criação de uma navegação entre as telas

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {NativeBaseProvider, StatusBar  } from 'native-base';
+
+import {TEMAS} from './src/estilos/Temas'
+import Rotas from './src/Rotas';
+
+export default function App() {
+  return (
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]}/>
+      <Rotas/>
+    </NativeBaseProvider>
+    );
+}
+
